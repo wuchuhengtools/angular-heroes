732804239b7e661aabbfd00e98aa1a11790e58b8
wuchuheng <wuchuheng@163.com>
2020-07-17 16:19:21 +0800

    添加heroes service 服务

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES} from '../mock-heroes';
+import { HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,14 +13,21 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Winstorm'
   };
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
+
   onSelect(hero: Hero): void
   {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void
+  {
+    this.heroService.getHeroes().subscribe( heros => this.heroes = heros);
   }
 }
