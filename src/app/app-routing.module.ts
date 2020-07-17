290a99caece6025f1b65df91d1f45e99b3990c09
wuchuheng <wuchuheng@163.com>
2020-07-18 02:32:32 +0800

    添加控制面板

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HeroesComponent} from './heroes/heroes.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashborad', pathMatch: 'full' },
+  { path: 'dashborad', component: DashboardComponent},
+  { path: 'heroes', component: HeroesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  public heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  public getHeroes(): void
+  {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes.slice(1, 5));
+  }
+}
