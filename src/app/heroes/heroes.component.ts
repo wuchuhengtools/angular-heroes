dea6da931ee305bff832788303b46274b46befd2
wuchuheng <wuchuheng@163.com>
2020-07-17 15:24:36 +0800

    为heros组件加样式和mock数据

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -11,9 +12,14 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Winstorm'
   };
+  heroes = HEROES;
+  selectHero: Hero;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onSelect(hero: Hero): void
+  {
+    this.selectHero = hero;
+  }
 }
